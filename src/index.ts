2741afa0351d5263d587260ea9e72b72a30611a3
sera1mu <ifrea0281@gmail.com>
2021-03-20 11:00:50 +0900

    🌱 Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+import { VERSION } from './util/constants';
+
+process.title = 'Ayanet';
+console.log('                                 _   ');
+console.log('     /\\                         | |  ');
+console.log('    /  \\  _   _  __ _ _ __   ___| |_ ');
+console.log("   / /\\ \\| | | |/ _` | '_ \\ / _ \\ __|");
+console.log('  / ____ \\ |_| | (_| | | | |  __/ |_ ');
+console.log(' /_/    \\_\\__, |\\__,_|_| |_|\\___|\\__|');
+console.log('          __/ |                     ');
+console.log('          |___/                      \n');
+console.log(`Ayanet v${VERSION} - Starting server...`);
