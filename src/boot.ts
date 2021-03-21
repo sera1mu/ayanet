90850f0aaebe6dac9f36310bd8a1975620f664b7
sera1mu <ifrea0281@gmail.com>
2021-03-21 15:35:10 +0900

    🥅 Throw when not specified CONFIG_PATH

--- src/boot.ts
+++ src/boot.ts
@@ -22,6 +22,10 @@ export default async function boot(): Promise<Discord.Client> {
   console.log('          __/ |                     ');
   console.log(`          |___/                      v${VERSION}\n`);
 
+  if (typeof process.env.CONFIG_PATH === 'undefined') {
+    throw new Error('Not specified CONFIG_PATH in environment variables.');
+  }
+
   // Load config
   const config = new Config(process.env.CONFIG_PATH);
 
