84f090604aa9e48273be93ff61cf0088242af799
sera1mu <ifrea0281@gmail.com>
2021-04-07 18:03:45 +0900

    🔥 Remove trimIndent function from misc utilties
    使われていないので削除

--- src/util/misc.ts
+++ src/util/misc.ts
@@ -1,14 +1,3 @@
-export const trimIndent = function trimIndentFromString(
-  ...args: string[]
-): string {
-  const raw = String.raw.apply(null, args);
-  return raw
-    .split('\n')
-    .map((s) => s.trim())
-    .join('\n')
-    .replace(/(^\n)|(\n$)/g, '');
-};
-
 export const generateDatetime = function generateDatetimeFromDate(
   date: Date
 ): string {
